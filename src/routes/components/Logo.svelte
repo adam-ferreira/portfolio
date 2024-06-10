431e686a23b4fd7869b2808a5b57e1a46f4f1946
adam-ferreira <75702894+adam-ferreira@users.noreply.github.com>
2024-06-10 18:38:51 +0200

    did some refactoring

--- src/routes/components/Logo.svelte
+++ src/routes/components/Logo.svelte
@@ -2,9 +2,7 @@
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-	import '.././styles.css';
-	import { loadTextures } from '../modules/textures.js';
-	import { addLighting } from '../modules/lighting.js';
+	import '../styles.css';
 
 	const CAMERA_CONFIG = {
 		NEAR: 0.1,
@@ -13,14 +11,14 @@
 	};
 
 	const SCALE_FACTOR = 45;
-
 	const orange = 'rgb(255, 116, 82)';
+	const AMBIENT_LIGHT_COLOR = 0xffffff;
+	const AMBIENT_LIGHT_INTENSITY = 5;
 
 	let container;
 	let adamLogo;
 
-	// This function sets up the 3D environment
-	const setupEnvironment = () => {
+	onMount(() => {
 		const aspect = container.clientWidth / container.clientHeight;
 		const camera = new THREE.OrthographicCamera(
 			-aspect,
@@ -31,20 +29,25 @@
 			CAMERA_CONFIG.FAR
 		);
 		camera.position.z = CAMERA_CONFIG.POSITION_Z;
+
 		const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 		renderer.setPixelRatio(window.devicePixelRatio);
 		renderer.setSize(container.clientWidth, container.clientHeight);
 		container.appendChild(renderer.domElement);
+
 		const scene = new THREE.Scene();
+		new THREE.CubeTextureLoader()
+			.setPath('cubeMaps/')
+			.load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'], (texture) => {
+				scene.environment = texture;
+			});
 
-		return { camera, renderer, scene };
-	};
+		const ambientLight = new THREE.AmbientLight(AMBIENT_LIGHT_COLOR, AMBIENT_LIGHT_INTENSITY);
+		scene.add(ambientLight);
 
-	// This function loads the 3D model
-	const loadModel = (scene) => {
 		const loader = new GLTFLoader();
-		loader.load('models/adam_ferreira_logo.glb', (gtlf) => {
-			adamLogo = gtlf.scene;
+		loader.load('models/adam_ferreira_logo.glb', (gltf) => {
+			adamLogo = gltf.scene;
 			const scale = container.clientWidth / SCALE_FACTOR;
 			adamLogo.scale.set(scale, scale, scale);
 			scene.add(adamLogo);
@@ -57,59 +60,38 @@
 				}
 			});
 		});
-	};
 
-	// This function handles the animation of the scene
-	const animate = (renderer, scene, camera) => {
 		const animate = () => {
 			requestAnimationFrame(animate);
 			renderer.render(scene, camera);
 		};
 		animate();
-	};
 
-	// This function handles window resize events
-	const onWindowResize = (camera, renderer) => {
-		const aspect = container.clientWidth / container.clientHeight;
-		camera.left = -aspect;
-		camera.right = aspect;
-		camera.top = 1;
-		camera.bottom = -1;
-		camera.updateProjectionMatrix();
-		renderer.setSize(container.clientWidth, container.clientHeight);
-	};
+		const onWindowResize = () => {
+			const aspect = container.clientWidth / container.clientHeight;
+			camera.left = -aspect;
+			camera.right = aspect;
+			camera.top = 1;
+			camera.bottom = -1;
+			camera.updateProjectionMatrix();
+			renderer.setSize(container.clientWidth, container.clientHeight);
+		};
 
-	const onMouseMove = (event) => {
-		const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-		const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+		const onMouseMove = (event) => {
+			const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+			const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+			const damping = 0.1;
 
-		const maxRotationX = Math.PI / 4;
-		const maxRotationY = Math.PI / 10;
+			adamLogo.rotation.x += (-mouseY - adamLogo.rotation.x) * damping;
+			adamLogo.rotation.y += (mouseX - adamLogo.rotation.y) * damping;
+		};
 
-		const targetRotationX = -mouseY * maxRotationY;
-		const targetRotationY = mouseX * maxRotationX;
+		window.addEventListener('resize', onWindowResize);
+		window.addEventListener('mousemove', onMouseMove);
 
-		const damping = 0.1;
-
-		adamLogo.rotation.x += (targetRotationX - adamLogo.rotation.x) * damping;
-		adamLogo.rotation.y += (targetRotationY - adamLogo.rotation.y) * damping;
-	};
-
-	onMount(() => {
-		const { camera, renderer, scene } = setupEnvironment();
-
-		loadTextures(scene);
-		addLighting(scene);
-
-		loadModel(scene);
-		animate(renderer, scene, camera);
-
-		window.addEventListener('resize', () => onWindowResize(camera, renderer));
-		window.addEventListener('mousemove', (event) => onMouseMove(event));
-
-		// Remove event listeners when the component is unmounted to prevent memory leaks
 		return () => {
-			window.removeEventListener('resize', () => onWindowResize(camera, renderer));
+			window.removeEventListener('resize', onWindowResize);
+			window.removeEventListener('mousemove', onMouseMove);
 		};
 	});
 </script>
